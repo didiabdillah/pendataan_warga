901223a194e525672727e82b7a3ebdab46545bd4
Didi Abdillah HMDev <didiabdillah@users.noreply.github.com>
2021-03-24 17:01:20 +0700

    SQL Update (2021-03-24)

---
+++ SQL Backup/pendataan_warga (2021-03-24).sql
@@ -0,0 +1,175 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 24, 2021 at 10:59 AM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 8.0.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pendataan_warga`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `forgot_tokens`
+--
+
+CREATE TABLE `forgot_tokens` (
+  `forgot_token_id` bigint(20) UNSIGNED NOT NULL,
+  `forgot_token_user_id` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `forgot_token_user_email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `forgot_token` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `forgot_token_due_time` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(1, '2021_03_23_065836_create_users_table', 1),
+(2, '2021_03_23_073037_create_reports_table', 1),
+(3, '2021_03_23_073713_create_forgot_tokens_table', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reports`
+--
+
+CREATE TABLE `reports` (
+  `report_id` bigint(20) UNSIGNED NOT NULL,
+  `report_nik` varchar(16) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `report_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `report_sex` tinyint(1) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `reports`
+--
+
+INSERT INTO `reports` (`report_id`, `report_nik`, `report_name`, `report_sex`, `created_at`, `updated_at`) VALUES
+(1, '3212132507010001', 'Didi Abdillah', 0, '2021-03-24 00:56:12', '2021-03-24 00:56:12'),
+(2, '3212132507010001', 'Chloe', 1, '2021-03-24 01:22:55', '2021-03-24 01:22:55');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `user_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `user_email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `user_password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `user_phone` varchar(16) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `user_role` enum('admin') COLLATE utf8mb4_unicode_ci NOT NULL,
+  `user_image` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `user_name`, `user_email`, `user_password`, `user_phone`, `user_role`, `user_image`, `created_at`, `updated_at`) VALUES
+('a5c860decf204ea686aedbb6dbeb6ab1', 'Didi', 'abdillah1965didiokey@gmail.com', '$2y$10$5mWQmCORH0ZXwJyPSicnde7r1hF0EndXUfwzmhWIGwTnnl6Qw.F0q', '087727598653', 'admin', 'default.jpg', '2021-03-24 00:55:15', '2021-03-24 00:55:15');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `forgot_tokens`
+--
+ALTER TABLE `forgot_tokens`
+  ADD PRIMARY KEY (`forgot_token_id`),
+  ADD KEY `forgot_tokens_forgot_token_user_id_foreign` (`forgot_token_user_id`);
+
+--
+-- Indexes for table `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `reports`
+--
+ALTER TABLE `reports`
+  ADD PRIMARY KEY (`report_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `users_user_email_unique` (`user_email`),
+  ADD UNIQUE KEY `users_user_phone_unique` (`user_phone`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `forgot_tokens`
+--
+ALTER TABLE `forgot_tokens`
+  MODIFY `forgot_token_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `reports`
+--
+ALTER TABLE `reports`
+  MODIFY `report_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `forgot_tokens`
+--
+ALTER TABLE `forgot_tokens`
+  ADD CONSTRAINT `forgot_tokens_forgot_token_user_id_foreign` FOREIGN KEY (`forgot_token_user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
